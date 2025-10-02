efc00da0d10605790089139c87b789ac2468723d
sandeepsvcn <sandeep@svcn.ac.in>
2025-10-02 11:41:04 +0530

    condition

--- shell-practice/condition.sh
+++ shell-practice/condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 NUMBER=$1
 
-if[$NUMBER -lt 10]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given Number $NUMBER is less than 10"
 else
     echo "Given Number $NUMBER is greater than 10"
