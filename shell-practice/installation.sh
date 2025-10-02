930cfa0e92797537cec20af0b34ae94001a3e048
sandeepsvcn <sandeep@svcn.ac.in>
2025-10-02 14:03:39 +0530

    condition

--- shell-practice/installation.sh
+++ shell-practice/installation.sh
@@ -7,15 +7,26 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
+checkpackage(){
+
+}
+
 validatesandeep(){
     if [ $1 -ne 0 ]; then
         echo "install error $2"
-        exit 1
+        exit 1 
     else
         echo "install sucess $2"
     fi
 }
 
+
+dnf list install mysql 
+validatesandeep $? "mysql"
+
+
+dnf list install python3 
+validatesandeep $? "python3"
 
 dnf install mysql -y
 
