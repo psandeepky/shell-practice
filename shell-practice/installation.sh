a6791c1c159ab5f0802eb261d4ea0a4441a50967
sandeepsvcn <sandeep@svcn.ac.in>
2025-10-02 13:42:27 +0530

    condition

---
+++ shell-practice/installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR"
+    exit 1
+if
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "error"
+    exit 1
+else
+    echo "sucess"
+if
